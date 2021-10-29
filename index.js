8ad292a29ab6c80305a155d1c27ff62958309f47
Joel <joellovesa380@gmailcom>
2021-10-29 17:57:20 +0530

    Configured MySQL with Node and updated README on instruction on how to configure MySQL for Node.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const express = require('express')
+const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
-const port = 3000
+// Required for environment variables (.env) which contain various credentials
+require('dotenv').config()
+
+const port = 3000;
 app.listen(port, () => {
     console.log("Server running on port " + port);
-})
+});
 
+// MySQL configuration
+var con = mysql.createConnection({
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USERNAME,
+    password: process.env.SQL_PASSWORD,
+    port: process.env.SQL_PORT
+});
+
+// Connecting to MySQL with the above credentials
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+// Accepting GET requests on this "" route
 app.get("", (req, res) => {
-    res.send("<p>Server is up and running</p>")
-})
+    res.send("<p>Server is up and running</p>");
+});
