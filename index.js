41cc4731814d4a224986e8e0e6145bb3588d6732
Joel <joellovesa380@gmailcom>
2021-11-01 09:42:11 +0530

    Creates database and table if it does not exist. Accepts get requests on /pasteId and sends JSON of the paste as response.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.listen(port, () => {
 });
 
 // MySQL configuration
-var con = mysql.createConnection({
+let con = mysql.createConnection({
     host: process.env.SQL_HOST,
     user: process.env.SQL_USERNAME,
     password: process.env.SQL_PASSWORD,
@@ -22,9 +22,31 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    // Creating database if it doesn't exist
+    const createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS pastedb";
+    con.query(createDatabaseQuery, function (err, result) {
+        if (err) throw err;
+        console.log("Database ready");
+    });
+    // Switching to the database to create table
+    con.query("USE pastedb")
+    // Creating the table if it doesn't exist
+    const createTableQuery = "CREATE TABLE IF NOT EXISTS pastes (id VARCHAR(64), paste LONGTEXT CHARACTER SET utf8);";
+    con.query(createTableQuery, function (err, result) {
+        if (err) throw err;
+        console.log("Table ready");
+    });
 });
 
-// Accepting GET requests on this "" route
-app.get("", (req, res) => {
-    res.send("<p>Server is up and running</p>");
+
+// Accepting GET requests on this "/pasteId" route which will return object where the pasteId matches the route
+app.get("/:pasteId", (req, res) => {
+    // Getting the paste from the database
+    const SQLQuery = "SELECT * FROM pastes WHERE id=" + req.params.pasteId;
+    con.query(SQLQuery, function (err, result, fields) {
+        if (err)
+            throw err;
+        // Sending the encrypted paste to the frontend
+        res.send(result);
+    });
 });
